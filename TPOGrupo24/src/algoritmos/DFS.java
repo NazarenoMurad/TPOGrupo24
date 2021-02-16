28a69385a1ac71031674cd9f4ec0ebccbf20eaa6
murad <murad@DESKTOP-HQVED8I.fibertel.com.ar>
2021-02-16 17:37:00 -0300

    Subo algoritmo DFS, a su vez hice cambios en la implementacion del grafo

--- TPOGrupo24/src/algoritmos/DFS.java
+++ TPOGrupo24/src/algoritmos/DFS.java
@@ -1,12 +1,43 @@
 package algoritmos;
 
+import implementaciones.ConjuntoEstatico;
 import implementaciones.ConjuntoTDA;
-import implementaciones.GrafosTDA;
+import implementaciones.GrafoTDA;
+
 
 public class DFS {
 	
-	public static void visitados (GrafosTDA grafo ) {
-		
+	public void  dfs (GrafoTDA grafo, int nodoAct, ConjuntoTDA visitados ) {
+		visitados.Agregar(nodoAct);
+		System.out.println(nodoAct);
+		ConjuntoTDA adyacentes=Adyacentes(grafo,nodoAct); //voy a buscar los adyacentes del nodoAct
+		while(!adyacentes.ConjuntoVacio()) {
+			int n=adyacentes.Elegir();
+			adyacentes.Sacar(n);
+			if(!visitados.Pertenece(n)) {
+				dfs(grafo,n,visitados);
+			}
+		}
+	}
+
+	public ConjuntoTDA Adyacentes(GrafoTDA grafo, int nodoAct) {
+	     ConjuntoTDA vertices = new ConjuntoEstatico(); 
+	     vertices.InicializarConjunto();
+	     ConjuntoTDA vAdyacentes = new ConjuntoEstatico(); 
+	     vAdyacentes.InicializarConjunto();
+	     vertices = grafo.Vertices();
+	     if (vertices.Pertenece(nodoAct)){
+	           vertices.Sacar(nodoAct);
+	     while (!vertices.ConjuntoVacio()){
+	             int vertice = vertices.Elegir();
+	             vertices.Sacar(vertice);
+	             if (grafo.ExisteArista(nodoAct, vertice)) {
+	                  vAdyacentes.Agregar(vertice);
+	            }
+	               
+	            }
+	        }
+	        return vAdyacentes;
 	}
 
 }
---
+++ TPOGrupo24/src/resources/PruebaDFS.java
@@ -0,0 +1,50 @@
+package resources;
+
+import algoritmos.DFS;
+import implementaciones.ConjuntoEstatico;
+import implementaciones.ConjuntoTDA;
+import implementaciones.GrafoEstatico;
+import implementaciones.GrafoTDA;
+import implementaciones.GrafosTDA;
+import implementaciones.ImplemEstatica;
+
+public class PruebaDFS {
+
+	public static void main(String[] args) {
+		ConjuntoTDA visitados=new ConjuntoEstatico();
+		visitados.InicializarConjunto();
+		GrafoTDA grafo=new GrafoEstatico();
+		grafo.InicializarGrafo();
+		grafo.AgregarVertice(0);
+		grafo.AgregarVertice(1);
+		grafo.AgregarVertice(2);
+		grafo.AgregarVertice(3);
+		grafo.AgregarVertice(4);
+		grafo.AgregarVertice(5);
+		grafo.AgregarArista(0, 1, 1);
+		grafo.AgregarArista(0, 2, 1);
+		grafo.AgregarArista(0, 3, 1);
+		grafo.AgregarArista(0, 4, 1);
+		grafo.AgregarArista(1, 2, 1);
+		grafo.AgregarArista(1, 5, 1);
+		grafo.AgregarArista(2, 3, 1);
+		grafo.AgregarArista(2, 4, 1);
+		grafo.AgregarArista(3, 4, 1);
+		grafo.AgregarArista(5, 4, 1);
+		
+		
+		DFS alg=new DFS();
+		alg.dfs(grafo, 0, visitados);
+		
+		/*ConjuntoTDA ady=new ConjuntoEstatico();
+		ady.InicializarConjunto();
+		ady=alg.Adyacentes(grafo, 0);
+		while(!ady.ConjuntoVacio()) {
+			int a=ady.Elegir();
+			System.out.println(a);
+			ady.Sacar(a);
+			
+		}*/
+	}
+
+}
