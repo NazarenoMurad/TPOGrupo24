9594e11a7c3ced67887793bf022cd8ec50ad7a16
murad <murad@DESKTOP-HQVED8I.fibertel.com.ar>
2021-02-16 16:27:07 -0300

    Subo implementacion de un conjunto estatico

---
+++ TPOGrupo24/src/implementaciones/ConjuntoTDA.java
@@ -0,0 +1,10 @@
+package implementaciones;
+
+public interface ConjuntoTDA {
+	public void Agregar( int x);
+	public boolean ConjuntoVacio();
+	public int Elegir();
+	public void InicializarConjunto();
+	public boolean Pertenece( int x);
+	public void Sacar( int x);
+}
